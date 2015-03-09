f6e66bf3d7d723188cc8defc8b90623d691afb22
Jason Noble <github+jasonn@jasonnoble.org>
2015-03-08 19:58:16 -0600

    Fixed extra spacing in mad lib output
    [#89879528]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -10,7 +10,7 @@ A vacation is when you take a trip to some #{ask_for('an adjective')} place
 with your #{ask_for('another adjective')} family. Usually you go to some place
 that is near a/an #{ask_for('a noun')} or up on a/an #{ask_for('another noun')}.
 A good vacation place is one where you can ride #{ask_for('a plural noun')}
-or play #{ask_for('a game')} or go hunting for #{ask_for('another plural noun')} . I like
+or play #{ask_for('a game')} or go hunting for #{ask_for('another plural noun')}. I like
 to spend my time #{ask_for('a verb ending in "ing"')} or #{ask_for('a verb ending in "ing"')}.
 When parents go on a vacation, they spend their time eating
 three #{ask_for('another plural noun')} a day, and fathers play golf, and mothers
